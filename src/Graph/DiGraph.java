10df6a2f6da5103c22cd2e9f4e60aca88179f81b
Eeshan Jaiswal <esjaiswal1@sheffield.ac.uk>
2019-03-12 08:06:30 +0800

    Bipartite check

--- src/Graph/DiGraph.java
+++ src/Graph/DiGraph.java
@@ -1,5 +1,6 @@
 package Graph;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 /*
@@ -13,6 +14,10 @@ public class DiGraph {
     int V ;
 
     // for bipartite graphs
+    /*
+    * It is not possible to detect bipartite with odd cycles with 2 colors
+    *
+    * */
     enum Color{ RED , BLUE};
 
     public DiGraph(int v){
@@ -57,6 +62,75 @@ public class DiGraph {
             }
         }
     }
+
+    /*
+    *
+    * using BFS and adjMatrix
+    * */
+    public boolean isBipartiteBFSAdjMatrix(int source , int[][] adjMatrix){
+        boolean[] visited = new boolean[V] ;
+        int[] colorArray = new int[V] ;
+        for(int counter = 0 ; counter< V ; counter++){ colorArray[counter] = -1 ; }
+        LinkedList<Integer> queue = new LinkedList<>() ;
+        visited[source] = true;
+
+        colorArray[source] = 1 ;
+        queue.add(source) ;
+
+        while(queue.size()!=0){
+            int i = queue.poll();
+            // if there are self loops, the graph is not bipartite
+            if(adjMatrix[i][i] ==1){return false ;}
+            // iterating over all the neighbors of the current vertex
+            for(int counter = 0 ; counter< V ; counter++){
+                // if the child vertex was unvisited till now and uncolored
+                if(adjMatrix[source][counter] ==1 && colorArray[counter]==-1){
+                    colorArray[counter] = 1 - colorArray[source] ;
+                    queue.add(counter);
+                }
+                // if the child node is visited and the color of the child is the same as
+                else if(adjMatrix[source][counter]==1 && colorArray[counter]== colorArray[source]){
+                    return false ;
+                }
+            }
+        }
+        // if we reach here, its a bipartite graph
+        return  true ;
+    }
+
+    /*
+    *
+    *
+    * */
+    public boolean isBipartiteBFSAdjList(int source , LinkedList[] adjList){
+        boolean[] visited = new boolean[V] ;
+        int[] colorArray = new int[V] ;
+        for(int i = 0; i<V ; i++){ colorArray[i] = -1; }
+        LinkedList<Integer> queue = new LinkedList<>();
+        visited[source] = true;
+        queue.add(source) ;
+
+        while(queue.size()!=0){
+            int nextSource = queue.poll()  ;
+            Iterator<Integer> it = adjList[nextSource].listIterator() ;
+            while(it.hasNext()){
+                int nextNeighbour=  it.next() ;
+                // check for self loops
+                if(nextNeighbour==nextSource)
+                        return false ;
+                if(!visited[nextNeighbour] && colorArray[nextNeighbour]==-1){
+                    colorArray[nextNeighbour]= 1 - colorArray[nextSource] ;
+                    queue.add(nextNeighbour) ;
+                }
+                else if(adjList[nextSource].contains(nextNeighbour) && colorArray[nextNeighbour]== colorArray[nextSource]) {
+                    return false;
+                }
+            }
+        }
+        return false ;
+    }
+
+
     public void DFS(int source){
         boolean[] visited = new boolean[V] ;
         DFSHelper(source, visited);
